c551e741c3e7bfe92c3d696b869c59d38d3830ab
maurobenitez <maurogabrielbenitez@gmail.com>
2023-07-24 15:00:42 -0300

    arreglado

--- src/middleware/adminMiddleware.js
+++ src/middleware/adminMiddleware.js
@@ -1,11 +1,22 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const JWT_secret = process.env.JWT_secret;
+
 const adminAuthMiddleware = (req, res, next) => {
-  const roleUser = req.headers.authorization;
-  console.log({ roleUser });
+  const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
 
-  if (roleUser !== 'admin')
-    return res.status(401).json({ message: 'Unauthorized access' });
-
-  next();
+  jwt.verify(token, JWT_secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+    if (decoded.role != 'admin'){
+      return res.status(401).json({ message: 'Invalid user' });
+    }
+    next();
+  });
 };
 
 module.exports = { adminAuthMiddleware };
